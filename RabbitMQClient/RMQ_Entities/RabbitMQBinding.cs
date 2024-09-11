f51e8987df6cb4285bbf007321e609a13ed7f1da
Michael Bohn <mjbohn@gmail.com>
2024-09-11 16:01:07 +0200

    Add folder for RMQ Entities Classes renamed to RabbitMQ<classname>

---
+++ RabbitMQClient/RMQ_Entities/RabbitMQBinding.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQClient.RMQ_Entities
+{
+    internal class RabbitMQBinding : TreeNode
+    {
+        public RabbitMQBinding() : base()
+        {
+            
+        }
+    }
+}
---
+++ RabbitMQClient/RMQ_Entities/RabbitMQExchange.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQClient.RMQ_Entities
+{
+    class RabbitMQExchange : TreeNode
+    {
+        public RabbitMQExchange() : base()
+        {
+
+        }
+
+
+    }
+}
