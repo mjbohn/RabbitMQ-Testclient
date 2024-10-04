03894e66d2ea3e22e9e0b1fa8d1cd6407388fef4
Michael Bohn <mjbohn@gmail.com>
2024-10-04 17:43:45 +0200

    Add properties to RabbitMQExchangeNode

--- RabbitMQClient/RMQ_Entities/RabbitMQExchange.cs
+++ RabbitMQClient/RMQ_Entities/RabbitMQExchange.cs
@@ -8,11 +8,25 @@ namespace RabbitMQClient.RMQ_Entities
 {
     class RabbitMQExchangeNode : TreeNode
     {
+        public string arguments {  get; set; }
+        public bool auto_delete { get; set; }
+        public bool durable { get; set; }
+        public bool isInternal {  get; set; }
+        public string type { get; set; }
+        public string user_who_performed_action { get; set; }
+        public string vhost { get; set; }
+        public string json_string { get; set; }
+
+
         public RabbitMQExchangeNode() : base()
         {
 
         }
 
+        public string ToString()
+        {
+            return json_string;
+        }
 
     }
 }
