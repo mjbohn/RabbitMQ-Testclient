e52f0bb50c9737a716fce4e97ccfaecb842e9b52
Michael Bohn <mjbohn@gmail.com>
2024-10-18 11:41:18 +0200

    Setting assembly version information and app-icon

--- RabbitMQClient/Program.cs
+++ RabbitMQClient/Program.cs
@@ -1,5 +1,22 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
+
+[assembly: AssemblyFileVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
+
+[assembly: AssemblyInformationalVersion(
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.Commits + "-" +
+    ThisAssembly.Git.Branch + "+" +
+    ThisAssembly.Git.Commit)]
+
+[assembly: AssemblyCopyright("Michael Bohn")]
+[assembly: AssemblyProduct("RabbitMQClient")]
+
 namespace RabbitMQClient
 {
+
     internal static class Program
     {
         /// <summary>
