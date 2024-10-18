7b8e62991fe5697b7badc9a0584a28e09353dceb
Michael Bohn <mjbohn@gmail.com>
2024-10-18 12:31:00 +0200

    add GlobalSupperssions.cs

---
+++ RabbitMQClient/GlobalSuppressions.cs
@@ -0,0 +1,18 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormMain.FormMain_FormClosing(System.Object,System.Windows.Forms.FormClosingEventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormVersionInfo.SetLabels")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormMain.buttonCreatePublisher_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormMain.#ctor")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormMain.buttonCreateConsumer_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormMain.closeToolStripMenuItem_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormMain.LabelVersionInfo_LinkClicked(System.Object,System.Windows.Forms.LinkLabelLinkClickedEventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.FormVersionInfo.buttonClose_Click(System.Object,System.EventArgs)")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.RMQ_Entities.RabbitMQBinding.#ctor")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.RMQ_Entities.RabbitMQExchangeNode.#ctor")]
+[assembly: SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>", Scope = "member", Target = "~M:RabbitMQClient.RMQ_Entities.RabbitMQVirtualHostNode.#ctor")]
