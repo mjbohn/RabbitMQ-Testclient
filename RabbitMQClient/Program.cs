296b4e505d01c00792f0d7aac487da1153e1b760
Michael Bohn <mjbohn@gmail.com>
2024-10-18 12:22:24 +0200

    bugfix version info

--- RabbitMQClient/Program.cs
+++ RabbitMQClient/Program.cs
@@ -1,12 +1,13 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
-
+//[assembly: AssemblyVersion(ThisAssembly.Git.BaseVersion.Major + "." + ThisAssembly.Git.BaseVersion.Minor + "." + ThisAssembly.Git.BaseVersion.Patch)]
 [assembly: AssemblyFileVersion(ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.SemVer.Patch)]
 
+
 [assembly: AssemblyInformationalVersion(
-    ThisAssembly.Git.SemVer.Major + "." +
-    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Major + "." +
+    ThisAssembly.Git.BaseVersion.Minor + "." +
+    ThisAssembly.Git.BaseVersion.Patch + "-" +
     ThisAssembly.Git.Commits + "-" +
     ThisAssembly.Git.Branch + "+" +
     ThisAssembly.Git.Commit)]
@@ -23,6 +24,7 @@ namespace RabbitMQClient
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Plattformkompatibilität überprüfen", Justification = "<Ausstehend>")]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
